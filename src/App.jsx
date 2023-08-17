cd95f42ab1b03471132daf9b6042a7bd76a4b873
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-08-17 13:58:15 +0600

    There were some issues with the ApiCall component. The API was already returning an array, but inside the setApiData function, you were passing data inside another array (setApiData([json])). This resulted in no data being available to display on the page. Therefore, I removed the extra array in the setApiData function. Additionally, the file was a JavaScript (.js) file, which was causing a compiler error. I changed the file extension to JSX (.jsx), and now everything is working correctly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import ApiCall from './components/ApiCall'
 
 function App() {
 
   return (
     <>
-     
+     <ApiCall />
     </>
   )
 }
